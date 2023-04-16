6fddc2fff933f12659e56d9d8acd040c3476c544
barnus <MolnBarn47@stud.uni-obuda.hu>
2023-04-16 13:46:55 +0200

    hazi06 started

--- HAZI/HAZI06/NJCleaner.py
+++ HAZI/HAZI06/NJCleaner.py
@@ -1,7 +1,7 @@
 import pandas as pd
+from datetime import datetime
 
 class NJCleaner():
-    
     
     
     def __init__(self,path):
@@ -10,5 +10,19 @@ class NJCleaner():
     def  order_by_scheduled_time(self):
         ordered=self.data.sort_values(by="scheduled_time")
         return ordered
+    
     def drop_columns_and_nan(self):
         self.data.drop(columns=["from","to"])
+    
+    def convert_date_to_day(self):
+        dates=pd.array(self.data["date"])
+        for item in dates:
+            datum=datetime.strptime(item,"%Y-%m-%d")
+            self.data = self.data.replace([item], datum.strftime("%A"))
+        return self.data
+
+
+path="C:/Tanulas/MI_specializáció/bevadat/files/2018_03.csv"
+njcleaner=NJCleaner(path)
+njcleaner.convert_date_to_day()      
+print(njcleaner.data)
