61c04cf7d2914978a078b99e7dd840dbf05c5107
barnus <MolnBarn47@stud.uni-obuda.hu>
2023-04-23 18:10:29 +0200

    linear regression finished

---
+++ GYAK/GYAK08/GYAK08.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class LinearRegression:
+
+    def __init__(self, epochs: int = 1000, lr: float = 1e-3):
+        self.epochs = epochs
+        self.lr = lr
+
+    def fit(self, X: np.array, y: np.array):
+        samples = X.shape[0]
+        features = X.shape[1]
+
+        self.weights = np.zeros(features + 1)
+
+        X = np.insert(X, 0, 1, axis=1)
+
+        for _ in range(self.epochs):
+            y_pred = np.dot(X, self.weights)
+            error = y - y_pred
+            gradient = -2 * np.dot(X.T, error) / samples
+            self.weights -= self.lr * gradient
+        
+    def predict(self, X):
+        X = np.insert(X, 0, 1, axis=1)
+        y_pred = np.dot(X, self.weights)
+        return y_pred
